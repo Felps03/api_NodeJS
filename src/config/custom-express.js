828bcfa2e134e4eb1c5e5b260ff825e308004c41
Felipe da Silva dos Santos <felipe.santos_BOLS@pampa.compasso>
2019-06-06 13:58:19 -0300

    header

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -8,6 +8,13 @@ app.use(bodyParser.json());
 
 app.use(expressValidator());
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    next();
+});
+
 const rotas = require('../app/rotas/rotas.js');
 rotas(app);
 
